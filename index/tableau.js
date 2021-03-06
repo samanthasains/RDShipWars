5352f5b1b05b38bfda97aa39f59a50641c812431
Christine Magnotta <christinemagnotta@CHRISTINEs-MacBook-Air.local>
2021-03-06 12:22:56 -0600

    redo

--- index/tableau.js
+++ index/tableau.js
@@ -9,7 +9,7 @@ function initializeViz() {
     // JS object that points at empty div in the html
     var placeholderDiv = document.getElementById("tableauViz");
     // URL of the viz to be embedded
-    var url = "http://public.tableau.com/views/WorldIndicators/GDPpercapita";
+    var url = "https://public.tableau.com/shared/G45RKS5YB?:display_count=y&:origin=viz_share_link";
     // An object that contains options specifying how to embed the viz
     var options = {
       width: '600px',
