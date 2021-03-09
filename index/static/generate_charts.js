def59a0113d4535a7df47d70a31fe0e4b2cbd7ba
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-03-09 17:42:56 -0600

    fixing things 1

--- index/static/generate_charts.js
+++ index/static/generate_charts.js
@@ -82,9 +82,9 @@ function fetchData(fileArr) {
       }
       if(flagForRBA == true && flagForBlob == true && flagForVader == true) {
         generateBarChart(rbaPosAvg, rbaNegAvg, rbaNeuAvg, textBlobPolarity, textBlobSubjectivity, vaderPosAvg, vaderNegAvg, vaderNeuAvg);
-        generatePieChart(rbaPosAvg, rbaNegAvg, 'RBA pos', 'RBA neg', 'pie_chart_1');
-        generatePieChart(textBlobPolarity, vaderCompAvg, 'TextBlog polarity', 'Vader Compound', 'pie_chart_2');
-        generatePieChart(vaderPosAvg, vaderNegAvg, 'Vader pos', 'Vader neg', 'pie_chart_3');
+        generatePieChart(rbaPosAvg, rbaNegAvg, rbaNeuAvg, 'RBA pos', 'RBA neg', 'RBA neu', 'pie_chart_1');
+        generatePieChart(textBlobPolarity, vaderCompAvg, 0, 'TextBlog polarity', 'Vader Compound', 'neu', 'pie_chart_2');
+        generatePieChart(vaderPosAvg, vaderNegAvg, vaderNeuAvg, 'Vader pos', 'Vader neg', 'Vader neu', 'pie_chart_3');
         // set values back to 0
         rbaPosAvg = 0;
         rbaNegAvg = 0;
@@ -94,6 +94,7 @@ function fetchData(fileArr) {
         vaderPosAvg = 0;
         vaderNegAvg = 0;
         vaderNeuAvg = 0;
+        vaderCompAvg = 0;
         totalCount = 0;
       }
       // generateBarChart(rbaPosAvg, rbaNegAvg, rbaNeuAvg);
@@ -120,107 +121,107 @@ function fetchTop5(fileLocation) {
   });
 }
 
-// function fetchAll(fileArray) {
-//   var varchieData = [];
-//   var varchiePos = 0;
-//   var varchieNeg = 0;
-//   var varchieNeu = 0;
-//   var bugheadData = [];
-//   var bugheadPos = 0;
-//   var bugheadNeg = 0;
-//   var bugheadNeu = 0;
-//   var barchieData = [];
-//   var barchiePos = 0;
-//   var barchieNeg = 0;
-//   var barchieNeu = 0;
-//   var choniData = [];
-//   var choniPos = 0;
-//   var choniNeg = 0;
-//   var choniNeu = 0;
-//   var shipType;
-//   fileArr.forEach(function(fileLocation) {
-//     // if (fileLocation.includes('varchie')) {
-//     //   console.log('hit 1');
-//     //   shipType = 'varchie';
-//     // } else if (fileLocation.includes('bughead')) {
-//     //   console.log('hit 2');
-//     //   shipType = 'textBlob';
-//     // } else if (fileLocation.includes('barchie')) {
-//     //   console.log('hit 3');
-//     //   analysisType = 'textBlob';
-//     // } else if (fileLocation.includes('choni')) {
-//     //   console.log('hit 3');
-//     //   shipType = 'textBlob';
-//     // }
-//     d3.csv(fileLocation).then(function(data) {
-//       data.filter(function(d, i) {
-//         if (fileLocation.includes('varchie')) {
-//           console.log('hit 1');
-//           shipType = 'varchie';
-//         } else if (fileLocation.includes('bughead')) {
-//           console.log('hit 2');
-//           shipType = 'textBlob';
-//         } else if (fileLocation.includes('barchie')) {
-//           console.log('hit 3');
-//           analysisType = 'textBlob';
-//         } else if (fileLocation.includes('choni')) {
-//           console.log('hit 3');
-//           shipType = 'textBlob';
-//         }
-//         vaderPosAvg = vaderPosAvg + parseFloat(d.pos);
-//         vaderNegAvg = vaderPosAvg + parseFloat(d.neg);
-//         vaderNeuAvg = vaderNeuAvg + parseFloat(d.neu);
-//         totalCount = totalCount+1;
-//       });
-//     }).then(function(data) {
-//       if (analysisType == 'rba') {
-//         console.log('hit rba');
-//         rbaPosAvg = rbaPosAvg/totalCount;
-//         rbaNegAvg = rbaNegAvg/totalCount;
-//         rbaNeuAvg = rbaNeuAvg/totalCount;
-//         totalCount = 0;
-//         flagForRBA = true;
-//         console.log('rbaPosAvg: '+rbaPosAvg+' rbaNegAvg: '+rbaNegAvg+' rbaNeuAvg: '+rbaNeuAvg);
-//       } else if (analysisType == 'textBlob') {
-//         console.log('hit textBlob');
-//         textBlobPolarity = textBlobPolarity/totalCount;
-//         textBlobSubjectivity = textBlobSubjectivity/totalCount;
-//         totalCount = 0;
-//         flagForBlob = true;
-//         console.log('textBlobPolarity: '+textBlobPolarity+' textBlobSubjectivity: '+textBlobSubjectivity);
-//       } else if (analysisType == 'vader') {
-//         console.log('hit vader');
-//         //Last analysis, so we have all the info so generateBarChart
-//         vaderPosAvg = vaderPosAvg/totalCount;
-//         vaderNegAvg = vaderNegAvg/totalCount;
-//         vaderNeuAvg = vaderNeuAvg/totalCount;
-//         console.log('vaderPosAvg: '+vaderPosAvg+' vaderNegAvg: '+vaderNegAvg+' vaderNeuAvg: '+vaderNeuAvg);
-//         flagForVader = true;
-//
-//       } else {
-//         // do nothing
-//       }
-//       if(flagForRBA == true && flagForBlob == true && flagForVader == true) {
-//         generateBarChart(rbaPosAvg, rbaNegAvg, rbaNeuAvg, textBlobPolarity, textBlobSubjectivity, vaderPosAvg, vaderNegAvg, vaderNeuAvg);
-//         generatePieChart(rbaPosAvg, rbaNegAvg, 'RBA pos', 'RBA neg', 'pie_chart_1');
-//         generatePieChart(textBlobPolarity, textBlobSubjectivity, 'TextBlog polarity', 'TextBlog subjectivity', 'pie_chart_2');
-//         generatePieChart(vaderPosAvg, vaderNegAvg, 'Vader pos', 'Vader neg', 'pie_chart_3');
-//         // set values back to 0
-//         rbaPosAvg = 0;
-//         rbaNegAvg = 0;
-//         rbaNeuAvg = 0;
-//         textBlobPolarity = 0;
-//         textBlobSubjectivity = 0;
-//         vaderPosAvg = 0;
-//         vaderNegAvg = 0;
-//         vaderNeuAvg = 0;
-//         totalCount = 0;
-//       }
-//       // generateBarChart(rbaPosAvg, rbaNegAvg, rbaNeuAvg);
-//
-//     });
-//   });
-// }
+function fetchAll(fileArray) {
+  var varchieData = [];
+  var varchiePos = 0;
+  var varchieNeg = 0;
+  var varchieNeu = 0;
+  var bugheadData = [];
+  var bugheadPos = 0;
+  var bugheadNeg = 0;
+  var bugheadNeu = 0;
+  var barchieData = [];
+  var barchiePos = 0;
+  var barchieNeg = 0;
+  var barchieNeu = 0;
+  var choniData = [];
+  var choniPos = 0;
+  var choniNeg = 0;
+  var choniNeu = 0;
+  var shipType;
+  fileArr.forEach(function(fileLocation) {
+    // if (fileLocation.includes('varchie')) {
+    //   console.log('hit 1');
+    //   shipType = 'varchie';
+    // } else if (fileLocation.includes('bughead')) {
+    //   console.log('hit 2');
+    //   shipType = 'textBlob';
+    // } else if (fileLocation.includes('barchie')) {
+    //   console.log('hit 3');
+    //   analysisType = 'textBlob';
+    // } else if (fileLocation.includes('choni')) {
+    //   console.log('hit 3');
+    //   shipType = 'textBlob';
+    // }
+    d3.csv(fileLocation).then(function(data) {
+      data.filter(function(d, i) {
+        if (fileLocation.includes('varchie')) {
+          console.log('hit 1');
+          vaderPosAvg = vaderPosAvg + parseFloat(d.pos);
+          vaderNegAvg = vaderPosAvg + parseFloat(d.neg);
+          vaderNeuAvg = vaderNeuAvg + parseFloat(d.neu);
+        } else if (fileLocation.includes('bughead')) {
+          console.log('hit 2');
+          shipType = 'textBlob';
+        } else if (fileLocation.includes('barchie')) {
+          console.log('hit 3');
+          analysisType = 'textBlob';
+        } else if (fileLocation.includes('choni')) {
+          console.log('hit 3');
+          shipType = 'textBlob';
+        }
+
+        totalCount = totalCount+1;
+      });
+    }).then(function(data) {
+      if (analysisType == 'rba') {
+        console.log('hit rba');
+        rbaPosAvg = rbaPosAvg/totalCount;
+        rbaNegAvg = rbaNegAvg/totalCount;
+        rbaNeuAvg = rbaNeuAvg/totalCount;
+        totalCount = 0;
+        flagForRBA = true;
+        console.log('rbaPosAvg: '+rbaPosAvg+' rbaNegAvg: '+rbaNegAvg+' rbaNeuAvg: '+rbaNeuAvg);
+      } else if (analysisType == 'textBlob') {
+        console.log('hit textBlob');
+        textBlobPolarity = textBlobPolarity/totalCount;
+        textBlobSubjectivity = textBlobSubjectivity/totalCount;
+        totalCount = 0;
+        flagForBlob = true;
+        console.log('textBlobPolarity: '+textBlobPolarity+' textBlobSubjectivity: '+textBlobSubjectivity);
+      } else if (analysisType == 'vader') {
+        console.log('hit vader');
+        //Last analysis, so we have all the info so generateBarChart
+        vaderPosAvg = vaderPosAvg/totalCount;
+        vaderNegAvg = vaderNegAvg/totalCount;
+        vaderNeuAvg = vaderNeuAvg/totalCount;
+        console.log('vaderPosAvg: '+vaderPosAvg+' vaderNegAvg: '+vaderNegAvg+' vaderNeuAvg: '+vaderNeuAvg);
+        flagForVader = true;
+
+      } else {
+        // do nothing
+      }
+      if(flagForRBA == true && flagForBlob == true && flagForVader == true) {
+        generateBarChart(rbaPosAvg, rbaNegAvg, rbaNeuAvg, textBlobPolarity, textBlobSubjectivity, vaderPosAvg, vaderNegAvg, vaderNeuAvg);
+        generatePieChart(rbaPosAvg, rbaNegAvg, 'RBA pos', 'RBA neg', 'pie_chart_1');
+        generatePieChart(textBlobPolarity, textBlobSubjectivity, 'TextBlog polarity', 'TextBlog subjectivity', 'pie_chart_2');
+        generatePieChart(vaderPosAvg, vaderNegAvg, 'Vader pos', 'Vader neg', 'pie_chart_3');
+        // set values back to 0
+        rbaPosAvg = 0;
+        rbaNegAvg = 0;
+        rbaNeuAvg = 0;
+        textBlobPolarity = 0;
+        textBlobSubjectivity = 0;
+        vaderPosAvg = 0;
+        vaderNegAvg = 0;
+        vaderNeuAvg = 0;
+        totalCount = 0;
+      }
+      // generateBarChart(rbaPosAvg, rbaNegAvg, rbaNeuAvg);
+
+    });
+  });
+}
 
 function init(ship) {
   rbaFile = "../rba files/"+ship+"_rba.csv";
--- index/static/pie_chart.js
+++ index/static/pie_chart.js
@@ -1,4 +1,4 @@
-function generatePieChart(positiveValue, negativeValue, field1, field2, pieChartName, titleName) {
+function generatePieChart(positiveValue, negativeValue, neutralValue, field1, field2, field3, pieChartName, titleName) {
   var pie_chart = c3.generate({
       bindto: '#'+pieChartName,
       data: {
@@ -6,6 +6,7 @@ function generatePieChart(positiveValue, negativeValue, field1, field2, pieChart
           columns: [
               [field1, positiveValue],
               [field2, negativeValue],
+              [field3, neutralValue]
           ],
           type : 'pie',
           //onclick: function (d, i) { console.log("onclick", d, i); },
@@ -19,7 +20,9 @@ function generatePieChart(positiveValue, negativeValue, field1, field2, pieChart
               // ratio = d3.format(",.2f")(ratio);
               // ratio = d3.format('%')(ratio);
               value = d3.format(",.3f")(value);
-              return [value];
+              ratio = d3.format('%')(ratio);
+              // ratio = d3.format('%')(ratio);
+              return [value, ratio];
             }
         }
       }
