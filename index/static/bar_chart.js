49ec3a08e8ea2be3c8709db856bc894ce05da9dd
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-03-09 03:29:35 -0600

    added vader files

--- index/static/bar_chart.js
+++ index/static/bar_chart.js
@@ -1,21 +1,24 @@
-var bar_chart = c3.generate({
-    bindto: '#bar_chart',
-    data: {
-        columns: [
-            ['RBA', 30, 200, 100, 400, 150, 250],
-            ['textBlob', 130, 100, 140, 200, 150, 50],
-            ['Vader NLTK', 130, 150, 200, 300, 200, 100]
-        ],
-        type: 'bar'
-    },
-    bar: {
-        width: {
-            ratio: 0.5 // this makes bar width 50% of length between ticks
-        }
-        // or
-        //width: 100 // this makes bar width 100px
-    }
-});
+function generateBarChart(rbaPosAvg, rbaNegAvg, rbaNeuAvg) {
+  var bar_chart = c3.generate({
+      bindto: '#bar_chart',
+      data: {
+          columns: [
+              ['RBA', rbaPosAvg, rbaNegAvg, rbaNeuAvg],
+              ['textBlob NLTK', rbaPosAvg, rbaNegAvg, rbaNeuAvg],
+              ['Vader NLTK', rbaPosAvg, rbaNegAvg, rbaNeuAvg]
+          ],
+          type: 'bar'
+      },
+      bar: {
+          width: {
+              ratio: 0.5 // this makes bar width 50% of length between ticks
+          }
+          // or
+          //width: 100 // this makes bar width 100px
+      }
+  });
+}
+
 
 // setTimeout(function () {
 //     bar_chart.load({
--- index/static/generate_charts.js
+++ index/static/generate_charts.js
@@ -1,61 +1,72 @@
 var rbaPosAvg = 0;
 var rbaNegAvg = 0;
 var rbaNeuAvg = 0;
-var textBlobPosAvg = 0;
-var textBlobNegAvg = 0;
-var textBlobNeuAvg = 0;
+var textBlobPolarity = 0;
+var textBlobSubjectivity = 0;
 var vaderPosAvg = 0.00;
 var vaderNegAvg = 0.00;
 var vaderNeuAvg = 0.00;
 var totalCount = 0;
+var analysisType;
 
-function fetchData(fileLocation, analysisType) {
-  d3.csv(fileLocation).then(function(data) {
-    data.filter(function(d, i) {
-      if(isNaN(d.pos) == false) {
-        // posVal = posVal.toFixed(2);
-        if (analysisType == 'rba') {
-          rbaPosAvg = rbaPosAvg + parseFloat(d.pos);
-          rbaNegAvg = rbaNegAvg + parseFloat(d.neg);
-          rbaNeuAvg = rbaNeuAvg + parseFloat(d.neu);
-        } else if (analysisType == 'textBlob') {
-
-        } else if (analysisType == 'vader') {
-
-        } else {
-          // do nothing
-        }
-
-      }
-
-
-      totalCount = totalCount+1;
-    });
-  }).then(function(data) {
-    // console.log('totalCount is: '+totalCount);
-    // console.log('PosAvg is: '+rbaPosAvg/totalCount);
-    // console.log('negAvg is: '+rbaNegAvg/totalCount);
-    // console.log('neuAvg is: '+rbaNeuAvg/totalCount);
-    // Get the averages
-    if (analysisType == 'rba') {
-      rbaPosAvg = rbaPosAvg/totalCount;
-      rbaNegAvg = rbaNegAvg/totalCount;
-      rbaNeuAvg = rbaNeuAvg/totalCount;
-    } else if (analysisType == 'textBlob') {
-      
-    } else if (analysisType == 'vader') {
-
-    } else {
-      // do nothing
+// function fetchData(fileLocation, analysisType) {
+function fetchData(fileArr) {
+  fileArr.forEach(function(fileLocation) {
+    if (fileLocation.includes('rba')) {
+      analysisType = 'rba';
+    } else if (fileLocation.includes('textBlob')) {
+      analysisType = 'textBlob';
+    } else if (fileLocation.includes('vader')) {
+      analysisType = 'vader';
     }
+    d3.csv(fileLocation).then(function(data) {
+      data.filter(function(d, i) {
+          // posVal = posVal.toFixed(2);
+          if (analysisType == 'rba') {
+            rbaPosAvg = rbaPosAvg + parseFloat(d.pos);
+            rbaNegAvg = rbaNegAvg + parseFloat(d.neg);
+            rbaNeuAvg = rbaNeuAvg + parseFloat(d.neu);
+          } else if (analysisType == 'textBlob') {
+            textBlobPolarity = textBlobPolarity + parseFloat(d.polarity);
+            textBlobSubjectivity = textBlobSubjectivity + parseFloat(d.subjectivity);
+          } else if (analysisType == 'vader') {
 
-  });
+          } else {
+            // do nothing
+          }
+        totalCount = totalCount+1;
+      });
+    }).then(function(data) {
+      // console.log('totalCount is: '+totalCount);
+      // console.log('PosAvg is: '+rbaPosAvg/totalCount);
+      // console.log('negAvg is: '+rbaNegAvg/totalCount);
+      // console.log('neuAvg is: '+rbaNeuAvg/totalCount);
+      // Get the averages
+      if (analysisType == 'rba') {
+        rbaPosAvg = rbaPosAvg/totalCount;
+        rbaNegAvg = rbaNegAvg/totalCount;
+        rbaNeuAvg = rbaNeuAvg/totalCount;
+      } else if (analysisType == 'textBlob') {
+
+      } else if (analysisType == 'vader') {
+
+      } else {
+        // do nothing
+      }
+      generateBarChart(rbaPosAvg, rbaNegAvg, rbaNeuAvg);
+    });
+  })
+
 }
 
 function init(ship) {
   rbaFile = "../rba files/"+ship+"_rba.csv";
-  fetchData(rbaFile, "rba");
-  textBlobFile = "";
+  textBlobFile = "../"+ship+"Blob.csv";
+  vaderFile = "../"+ship+"_vader.csv";
+  var fileArr = [rbaFile, textBlobFile];
+  fetchData(fileArr);
+  // fetchData(rbaFile, "rba");
+  // textBlobFile = "../"+ship+"Blob.csv";
   //fetchData(textBlobFile, "textBlob");
   vaderFile = "";
   //fetchData(vaderFile, "vader");
