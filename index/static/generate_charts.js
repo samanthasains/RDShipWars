abb3efdfddca0217fc52093e21688b509c51badb
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-03-08 19:29:35 -0600

    Fixed data type issue

--- index/static/generate_charts.js
+++ index/static/generate_charts.js
@@ -1,9 +1,9 @@
 var rbaPosAvg = 0;
 var rbaNegAvg = 0;
 var rbaNeuAvg = 0;
-var textBlobPosAvg = 0.00;
-var textBlobNegAvg = 0.00;
-var textBlobNeuAvg = 0.00;
+var textBlobPosAvg = 0;
+var textBlobNegAvg = 0;
+var textBlobNeuAvg = 0;
 var vaderPosAvg = 0.00;
 var vaderNegAvg = 0.00;
 var vaderNeuAvg = 0.00;
@@ -12,34 +12,30 @@ var totalCount = 0;
 function fetchData(fileLocation, analysisType) {
   d3.csv(fileLocation).then(function(data) {
     data.filter(function(d, i) {
-      //console.log(d.pos);
       if(isNaN(d.pos) == false) {
-        var posVal = d.pos;
-        posVal = posVal.toFixed(2);
+        // posVal = posVal.toFixed(2);
         if (analysisType == 'rba') {
-          rbaPosAvg = rbaPosAvg + posVal;
+          rbaPosAvg = rbaPosAvg + parseFloat(d.pos);
+          rbaNegAvg = rbaNegAvg + parseFloat(d.neg);
+          rbaNeuAvg = rbaNeuAvg + parseFloat(d.neu);
+        } else if (analysisType == 'textBlob') {
+
+        } else if (analysisType == 'vader') {
+
+        } else {
+          // do nothing
         }
+
       }
 
-      // switch(analysisType) {
-      //   case "rba":
-      //     //console.log(typeof d.pos);
-      //     //console.log(d.neg);
-      //     //console.log(d.neu);
-      //     rbaPosAvg = parseFloat(d.pos)+1;
-      //     rbaNegAvg = rbaNegAvg + parseFloat(d.neg);
-      //     rbaNeuAvg = rbaNeuAvg + parseFloat(d.neu);
-      //     break;
-      //   case "textBlob":
-      //     break;
-      //   case "vader":
-      //     break;
-      // }
+
       totalCount = totalCount+1;
     });
   }).then(function(data) {
     console.log('totalCount is: '+totalCount);
-    console.log(rbaPosAvg);
+    console.log('PosAvg is: '+rbaPosAvg/totalCount);
+    console.log('negAvg is: '+rbaNegAvg/totalCount);
+    console.log('neuAvg is: '+rbaNeuAvg/totalCount);
   });
 }
 
